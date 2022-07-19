4012449acb28263bfd92f888b7bbe9cfa89aa929
nayab jahan <nayabzafir@gmail.com>
2022-07-19 19:25:04 +0530

    Added:creating nodes

---
+++ src/main/java/com/blz/LinkedList/MyNode.java
@@ -0,0 +1,24 @@
+package com.blz.LinkedList;
+
+public class MyNode <K>{
+    private K key;
+    private MyNode next;
+
+    public K getKey() {
+        return key;
+    }
+    public void setKey(K key) {
+        this.key = key;
+    }
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public MyNode(K key) {
+        this.key = key;
+    }
+}
